73173bbf6bc0ad9d8f5fc5d8c3b987f58c5a7bec
John Krauss <irving.krauss@gmail.com>
2015-02-06 14:36:50 -0500

    enabling strip_indexes

--- conf.py
+++ conf.py
@@ -551,7 +551,7 @@ COMMENT_SYSTEM_ID = ""
 # it will instead /foo/default.html => /foo)
 # (Note: This was briefly STRIP_INDEX_HTML in v 5.4.3 and 5.4.4)
 # Default = False
-# STRIP_INDEXES = False
+STRIP_INDEXES = True
 
 # Should the sitemap list directories which only include other directories
 # and no files.
